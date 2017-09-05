204225fcd022b3892a2f290d1c18da314311feb8
Dmytro Portenko <troublesohard@gmail.com>
2017-09-05 23:47:05 +0300

    MMA-8: update cart image style

--- src/components/cart/HeaderCartButton.js
+++ src/components/cart/HeaderCartButton.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'react-native';
+import { Image, TouchableHighlight, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 class HeaderCartButton extends Component {
@@ -10,13 +10,44 @@ class HeaderCartButton extends Component {
 
 	render() {
 		return (
-				<Button
-						title={`Cart ${this.props.itemsCount}`}
-						onPress={this.onPress.bind(this)}
-				/>
+				<TouchableHighlight onPress={this.onPress.bind(this)}>
+					<Image
+							source={require('../../../resources/icons/shopping-cart.png')}
+							style={styles.iconStyle}
+					>
+						<View style={styles.textBackground}>
+								<Text style={styles.textStyle}>{this.props.itemsCount}</Text>
+						</View>
+					</Image>
+				</TouchableHighlight>
 		);
 	}
 }
+
+const styles = {
+	iconStyle: {
+		height: 32,
+		width: 32,
+		margin: 5,
+		paddingRight: 10,
+		marginRight: 10,
+	},
+	textBackground: {
+		backgroundColor: '#999',
+		height: 15,
+		width: 15,
+		borderRadius: 15,
+		marginLeft: 18,
+
+	},
+	textStyle: {
+		color: '#fff',
+		fontSize: 12,
+		textAlign: 'center',
+		backgroundColor: 'transparent'
+		// flexWrap: 'wrap'
+	}
+};
 
 const mapStateToProps = ({ cart }) => {
 	const itemsCount = cart.items ? cart.items.length : 0;
