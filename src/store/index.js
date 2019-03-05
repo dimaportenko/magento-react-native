fa1f6363944cbd57c3effe85bc68b386115fc60b
Artem Zhydkov <dtema1996@gmail.com>
2019-03-05 18:00:54 +0200

    remove debagging information

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import reducers from '../reducers';
 
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  {}, //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   compose(applyMiddleware(thunk))
 );
 
