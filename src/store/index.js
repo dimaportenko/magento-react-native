52bdc1ae70206a62b54fbcfadbeeda79483213c0
Artem Zhydkov <dtema1996@gmail.com>
2019-03-14 15:50:40 +0200

    Added persist logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
+
+
 import reducers from '../reducers';
 
-const store = createStore(
-  reducers,
-  {}, //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  compose(applyMiddleware(thunk))
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(
+  persistedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // TODO: remove for production
+  applyMiddleware(thunk)
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+// persistor.purge();
