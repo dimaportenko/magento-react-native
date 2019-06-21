a5af029f944778cf32951f6c6279e85ceecfdad0
Dmytro Portenko <troublesohard@gmail.com>
2019-06-21 13:14:52 +0300

    Update magento-sample.js
    remove admin auth from config sample

--- src/config/magento-sample.js
+++ src/config/magento-sample.js
@@ -2,11 +2,6 @@ export const magentoOptions = {
   url: 'http://mage2.local/',
   home_cms_block_id: '19',
   authentication: {
-    login: {
-      type: 'admin',
-      username: 'dev',
-      password: 'q1w2e3r4'
-    },
     integration: {
       access_token: 'cdlodltsj3wfwd1jrx08q9nfprb5idq4'
     }
