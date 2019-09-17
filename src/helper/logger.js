0f5d4b4e2352df1081b8901a64f5b012b1c7ab67
Dmytro Portenko <troublesohard@gmail.com>
2019-09-17 17:10:11 +0300

    bugsnag improve logging logic

--- src/helper/logger.js
+++ src/helper/logger.js
@@ -3,5 +3,9 @@ const bugsnag = new Client('f1e939802a0abb0832574304f55776a5');
 
 export const logError = (error) => {
   console.log(error);
-  bugsnag.notify(error);
+  if (error instanceof Error) {
+    bugsnag.notify(error);
+  } else if (typeof error === 'string') {
+    bugsnag.notify(new Error(error));
+  }
 };
