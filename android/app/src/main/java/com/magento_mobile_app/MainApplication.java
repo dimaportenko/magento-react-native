d62769f3dd53480cb104cbc946b14007c4d2e530
Dmytro Portenko <troublesohard@gmail.com>
2019-09-22 22:15:55 +0300

    add and link braintree module

--- android/app/src/main/java/com/magento_mobile_app/MainApplication.java
+++ android/app/src/main/java/com/magento_mobile_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.magento_mobile_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.pw.droplet.braintree.BraintreePackage;
 import com.bugsnag.BugsnagReactNative;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BraintreePackage(),
             BugsnagReactNative.getPackage(),
             new VectorIconsPackage()
       );
