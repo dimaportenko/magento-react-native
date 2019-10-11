9ec74644b9bad07782428e84ed4c25f711d5e046
Dmytro Portenko <troublesohard@gmail.com>
2019-10-11 18:07:26 +0300

    WIP: RN upgrade to 61.2

--- android/app/src/main/java/com/magento_mobile_app/MainApplication.java
+++ android/app/src/main/java/com/magento_mobile_app/MainApplication.java
@@ -3,8 +3,6 @@ package com.magento_mobile_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.bugsnag.BugsnagReactNative;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            BugsnagReactNative.getPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
 
