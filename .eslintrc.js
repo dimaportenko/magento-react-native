33fa21000bf51eb74ebbefb4cedd5eacee41fd62
Dmytro Portenko <troublesohard@gmail.com>
2019-11-03 14:11:49 +0200

    fix eslint config, fix custom options for simple products

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'object-curly-spacing': [
+      "error", "always", { "objectsInObjects": true }
+    ],
+    'prettier/prettier': 0,
+    "react/prop-types": 1
+  }
 };
