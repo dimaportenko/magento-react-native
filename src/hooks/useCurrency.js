45f5efb3f35b0d6004e74fdcc5de426b052b3d9b
Dmytro Portenko <troublesohard@gmail.com>
2020-05-14 18:15:19 +0300

    add currency hook

---
+++ src/hooks/useCurrency.js
@@ -0,0 +1,24 @@
+/**
+ * @flow
+ * Created by Dima Portenko on 14.05.2020
+ */
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+export const useCurrency = (props) => {
+  const { currencyRate, currencySymbol } = useSelector(state => {
+    const {
+      currency: {
+        displayCurrencySymbol: currencySymbol,
+        displayCurrencyExchangeRate: currencyRate,
+      },
+    } = state.magento;
+
+    return {
+      currencyRate,
+      currencySymbol,
+    };
+  });
+
+  return { currencyRate, currencySymbol };
+};
